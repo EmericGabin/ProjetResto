825b112c22cf93938e039cc7ec266d4cc56336ff
unknown <micmicgabin@gmail.com>
2022-01-10 00:55:04 +0100

    ajout des btns

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -5,8 +5,21 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+const $ = require("jquery");
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require("bootstrap");
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function () {
+  $('[data-toggle="popover"]').popover();
+});
+
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/global.scss';
+import "./styles/global.scss";
 
 // start the Stimulus application
-import './bootstrap';
+import "./bootstrap";
